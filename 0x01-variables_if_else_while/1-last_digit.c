cb6ba4c9df045db2305cd9a1fbfb32c0456e7d69
desupinho <supinhoking4ril@gmail.com>
2023-02-18 04:06:49 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - This program will assign a random number to the variable n each time it is executed.
+ * main - This program will assign random number to the variable n each time it is executed
  *
  * Return: Always 0 (success)
  */
