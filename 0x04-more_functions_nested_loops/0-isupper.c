28cc4b0905b32e299d08f456769fe474d74ea0d5
desupinho <supinhoking4ril@gmail.com>
2023-02-23 15:59:54 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,17 +4,11 @@
 * @c: Variable text
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
-
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
-{
- return (0);
-}
-
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
